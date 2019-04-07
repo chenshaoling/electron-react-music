e793b003d278ae5159eafdaa18294a5e43d3763d
chenshaoling <shijieqianbei@outlook.com>
2019-04-07 23:09:49 +0800

    add searchbcox

---
+++ src/components/SearchBox/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+// import * as Style from './index.scss';
+import * as classNames from 'classnames'
+import Styles from './index.scss';
+
+
+export interface Props {
+    className?: string;
+}
+export interface State{
+    
+}
+class SearchBox extends React.Component<Props, State> {
+    constructor (props: Props) {
+        super(props);
+        this.state = {
+            scrollToTop: false,
+        };
+    }
+
+    render(){
+        let className = classNames(this.props.className, Styles.sec);
+        return (
+            <div className={className}>
+                <input type="text" placeholder="搜索"/>
+            </div>
+        )
+    }
+}   
+export default SearchBox;
