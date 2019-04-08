bed301fa24458f5f3c0087bb55a079f8ddad5171
chenshaoling <shijieqianbei@outlook.com>
2019-04-08 23:46:01 +0800

    add swiper

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import * as classNames from 'classnames'
 import Styles from './index.scss';
 // import Swiper from 'react-id-swiper';
+import Swiper from 'react-id-swiper/lib/ReactIdSwiper.full';
+
+// import { Pagination, Navigation } from 'swiper/dist/js/swiper.esm'
 
 
 export interface Props {
@@ -10,11 +13,26 @@ export interface Props {
 export interface State{
     
 }
+
+// const params = {
+//     modules: [Pagination, Navigation],
+//     pagination: {
+//       el: '.swiper-pagination',
+//       type: 'bullets',
+//       clickable: true
+//     },
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev'
+//     },
+//     spaceBetween: 30
+//   }
 class Banner extends React.Component<Props, State> {
     constructor (props: Props) {
         super(props);
         this.state = {
         };
+        console.log(Swiper)
     }
     
     
@@ -24,13 +42,13 @@ class Banner extends React.Component<Props, State> {
 
         return (
             <div className={className}>
-                {/* <Swiper> */}
+                <Swiper>
                     <div>Slide 1</div>
                     <div>Slide 2</div>
                     <div>Slide 3</div>
                     <div>Slide 4</div>
                     <div>Slide 5</div>
-                {/* </Swiper> */}
+                </Swiper>
                 {/* <button onClick={this.goPrev.bind(this, swiper)}>Prev</button>
                 <button onClick={this.goNext.bind(this, swiper)}>Next</button> */}
             </div>
