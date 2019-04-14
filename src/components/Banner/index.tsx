b46b3f48ba22dc06ea817adace071c7f5a32d672
chenshaoling <shijieqianbei@outlook.com>
2019-04-14 23:53:17 +0800

    add swiper banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -40,71 +40,47 @@ class Banner extends React.Component<Props, State> {
     componentDidMount(){
         
         var swiper = new Swiper('.swiper-container', {
-        //   pagination: '.swiper-pagination',
-        //   nextButton: '.swiper-button-next',
-        //   prevButton: '.swiper-button-prev',
-        //   paginationClickable: true,
+          pagination: {
+            el:'.swiper-pagination',
+            clickable: true
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+          },
           spaceBetween: 30,
           centeredSlides: true,
-        //   autoplay: 2500,
-        //   autoplayDisableOnInteraction: false
+          autoplay: true,
+          loop : true,
+          effect : 'coverflow',
+          slidesPerView: 2,
+          coverflowEffect: {
+            rotate: 0,
+            stretch: 100,
+            depth: 100,
+            modifier: 2,
+            slideShadows : true
+          },
         });
       }
     
     render(){
         let className = classNames(this.props.className, Styles.sec);
-        // const [swiper, updateSwiper] = React.useState(null);
-
-        // React.useEffect(
-        //     () => {
-        //       if (swiper !== null) {
-        //         swiper.on("resize", () => {
-        //           swiper.params.centeredSlides = window.innerWidth < 900;
-        //           swiper.update();
-        //         });
-        //       }
-        //     },
-        //     [swiper]
-        // );
         return (
             <div className={className}>
               <div className="swiper-container">
                 <div className="swiper-wrapper">
-                    <div className="swiper-slide">Slide 1</div>
-                    <div className="swiper-slide">Slide 2</div>
-                    <div className="swiper-slide">Slide 3</div>
+                    <div className={classNames("swiper-slide", Styles.slider)}>Slide 1</div>
+                    <div className={classNames("swiper-slide", Styles.slider)}>Slide 2</div>
+                    <div className={classNames("swiper-slide", Styles.slider)}>Slide 3</div>
                 </div>
                 <div className="swiper-pagination"></div>
                 
                 <div className="swiper-button-prev"></div>
                 <div className="swiper-button-next"></div>
-                
-                <div className="swiper-scrollbar"></div>
             </div>
-               {/* <div className="swiper-container">
-                    <div className="swiper-wrapper">
-                        <div className="swiper-slide">Slide 1</div>
-                        <div className="swiper-slide">Slide 2</div>
-                        <div className="swiper-slide">Slide 3</div>
-                        <div className="swiper-slide">Slide 4</div>
-                    </div>
-                    <div className="swiper-pagination"></div>
-                    <div className="swiper-button-next"></div>
-                    <div className="swiper-button-prev"></div>
-                </div> */}
           </div>
         )
     }
-    goNext(swiper: any){
-        if (swiper !== null) {
-          swiper.slideNext();
-        }
-    };
-     
-    goPrev(swiper: any){
-        if (swiper !== null) {
-            swiper.slidePrev();
-        }
-    };
 }   
 export default Banner;
